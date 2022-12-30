892f344a48769faa9ccaaf3f3361d4eab9bc988c
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2022-12-31 00:33:29 +0530

    Added: Storing Flips in Dictionary and determining percentage of Doublet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,29 +1,40 @@
 read -p "Enter number of times to flip a combination:" nFlips
 
-declare -A singletFrequency
+declare -A doubletFrequency
 
-singletFrequency[H]=0
-singletFrequency[T]=0
+                   doubletFrequency[HH]=0
+                   doubletFrequency[HT]=0
+                   doubletFrequency[TH]=0
+                   doubletFrequency[TT]=0
 
-echo "Singlet Combination"
 for(( flip = 1; flip <= nFlips; flip++ ))
 do
     echo -n "Flip-$flip is "
-    (( toss = RANDOM % 2 ))
-    case $toss in
-        0)
-            echo "Heads"
-            (( singletFrequency[H]++ ))
+    (( coin_1 = RANDOM % 2 ))
+    (( coin_2 = RANDOM % 2 ))
+        case $coin_1$coin_2 in
+        00)
+            echo "Heads Heads"
+            (( doubletFrequency[HH]++ ))
             ;;
-        1)
-            echo "Tails"
-            (( singletFrequency[T]++ ))
+        01)
+            echo "Heads Tails"
+            (( doubletFrequency[HT]++ ))
+            ;;
+        10)
+            echo "Tails Heads"
+            (( doubletFrequency[TH]++ ))
+            ;;
+        11)
+            echo "Tails Tails"
+            (( doubletFrequency[TT]++ ))
+            ;;
     esac
 done
 
-for combination in ${!singletFrequency[@]}
+for combination in ${!doubletFrequency[@]}
 do
-    percentage=$(( ${singletFrequency[$combination]}  * 100 / nFlips ))
-    singletFrequency[$combination]=$percentage
-    echo "percentage of $combination is ${singletFrequency[$combination]}%"
+    percentage=$(( ${doubletFrequency[$combination]}  * 100 / nFlips ))
+    doubletFrequency[$combination]=$percentage
+    echo "percentage of $combination is ${doubletFrequency[$combination]}%"   
 done
