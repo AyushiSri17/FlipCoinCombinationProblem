906f9657eab6ee89eb0bbcfd8caeef9307c62c86
“AyushiSri17” <srivastavaayushi7110@gmail.com>
2022-12-31 00:18:50 +0530

    Added: Storing Flips in Dictionary and determining percentage of Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,7 +1,29 @@
-toss=$(( RANDOM % 2 ))
-if(( toss == 0 ))
-then
-    echo "Heads"
-else
-    echo "Tails"
-fi
+read -p "Enter number of times to flip a combination:" nFlips
+
+declare -A singletFrequency
+
+singletFrequency[H]=0
+singletFrequency[T]=0
+
+echo "Singlet Combination"
+for(( flip = 1; flip <= nFlips; flip++ ))
+do
+    echo -n "Flip-$flip is "
+    (( toss = RANDOM % 2 ))
+    case $toss in
+        0)
+            echo "Heads"
+            (( singletFrequency[H]++ ))
+            ;;
+        1)
+            echo "Tails"
+            (( singletFrequency[T]++ ))
+    esac
+done
+
+for combination in ${!singletFrequency[@]}
+do
+    percentage=$(( ${singletFrequency[$combination]}  * 100 / nFlips ))
+    singletFrequency[$combination]=$percentage
+    echo "percentage of $combination is ${singletFrequency[$combination]}%"
+done
